ac40e20b2aae3e702e28cfd6677030c86ef465f2
getdirty75 <benoit.raynaud95@gmail.com>
2020-12-06 19:25:06 +0100

    !core

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import store from './Store/store';
 
 import Login from './Stage/Feature/Login';
 import PrivateDealer from './Stage/Feature/Profile/Private/Dealer/index';
+import { Profile } from './Stage/Feature/Profile/Public';
 import { Home } from './Stage/Feature/Home';
 import GuestRoute from './Stage/Utils/GuestRoute';
 import PrivateRoute from './Stage/Utils/PrivateRoute';
@@ -19,7 +20,8 @@ const App: React.FC = () => {
       <Provider store={store}>
         <Router>
           <Switch>
-            <PrivateRoute exact path='/profile' component={PrivateDealer} />
+            <PrivateRoute exact path='/privateProfile' component={PrivateDealer} />
+            <GuestRoute exact path='/profile/:beatmakerId' component={Profile} />
             <GuestRoute exact path='/login' component={Login} />
             <GuestRoute exact path='/' component={Home} />
           </Switch>
---
+++ src/Stage/Data/model.ts
@@ -0,0 +1,98 @@
+import { Marker } from "react-globe";
+
+//#region INTERFACE
+
+export interface COUNTRY {
+  id: number;
+  flag?: {
+    file: File;
+    url: string;
+  };
+  marker: Marker;
+  beatmakers: USER['id'][];
+}
+
+export interface USER {
+  id: number;
+  createdAt?: Date;
+  account: {
+    mail: string;
+    password: string;
+    campaign: CampaignType;
+    role: UserRole;
+  };
+  info: {
+    firstName: string;
+    lastName: string;
+    username: string;
+    address: string;
+    country: COUNTRY['id'];
+    avatar?: {
+      file: File;
+      url: string;
+    }
+  };
+  beats: BEAT[];
+  followers: USER[];
+  plays: number;
+  reposts?: any;
+};
+
+export interface BEAT {
+  id: number;
+  createdAt: Date;
+  basics: {
+    duration: number;
+    name: string;
+    picture: {
+      file: File;
+      url: string;
+    };
+    user: USER;
+    price: number;
+    exclusive: boolean;
+  };
+  stats: {
+    played: number;
+    sold: number;
+    favorited: number;
+    reposted: number;
+  };
+  style: {
+    genre: BeatGenre;
+    mood: BeatMood;
+    relatedArtist: string;
+    tags: string[];
+  };
+}
+
+export interface CAMPAIGN {
+  id: number;
+  duration: number;
+  price: number;
+  type: CampaignType;
+}
+//#endregion
+
+//#region ENUM
+export enum UserRole {
+  BEATMAKER = 'Beatmaker',
+  CUSTOMER = 'Customer',
+  MASTER = 'Master',
+};
+export enum CampaignType {
+  FREE = 'Free',
+  BASIC = 'Basic',
+  KING = 'King',
+};
+export enum BeatGenre {
+  TRAP = 'Trap',
+  DRILL = 'Drill'
+}
+export enum BeatMood {
+  SHINE = 'Shine',
+  DARK = 'Dark',
+  SAD = 'Sad',
+  OTHER = 'Other'
+}
+//#endregion
